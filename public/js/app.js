3c46a925b7d6eca67fdccb459fa6297c2390c97e
bexruz-cyber <mayoq2024@gmail.com>
2024-11-23 21:01:05 +0500

    footer add

--- public/js/app.js
+++ public/js/app.js
@@ -10,3 +10,30 @@ hamburgerBtn.addEventListener("click", function () {
   hamburger.classList.toggle("hidden");
   close.classList.toggle("hidden");
 });
+
+let footerDropDown = document.querySelector("#footerDropDown");
+let footerThemeBtn = document.querySelector("#footerThemeBtn");
+let footerDropDownText = document.querySelector(".footerDropDownText");
+let footerDropDownTitle = document.querySelector("#footerDropDownTitle");
+
+// Dropdownni ochish-yopish
+footerThemeBtn.addEventListener("click", function () {
+  footerDropDown.classList.toggle("h-11");
+  footerDropDown.classList.toggle("h-[70px]");
+  footerDropDownText.classList.toggle("opacity-0");
+});
+
+// Faqat matnni almashtirish va dropdownni yopish
+footerDropDownText.addEventListener("click", function () {
+  if (footerDropDownTitle.textContent.trim() === "Темная тема") {
+    footerDropDownTitle.textContent = "Светлая тема";
+    footerDropDownText.textContent = "Темная тема.";
+  } else {
+    footerDropDownTitle.textContent = "Темная тема";
+    footerDropDownText.textContent = "Светлая тема.";
+  }
+
+  footerDropDown.classList.add("h-11");
+  footerDropDown.classList.remove("h-[70px]");
+  footerDropDownText.classList.add("opacity-0");
+});
